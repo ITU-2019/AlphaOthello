99892f5f959c1c23282c1e571cc61eed72510153
Wrede <sbwr@itu.dk>
2018-02-25 09:49:10 +0100

    Add OthelloAI21

---
+++ src/OthelloAI21.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+/**
+ * A super-complex OthelloAI-implementation using alpha-beta pruning. 
+ * @author Group21
+ * @version 9.2.2018
+ */
+public class OthelloAI21 implements IOthelloAI{
+
+	/**
+	 * Returns the optimal move decided by the AI
+	 */
+	public Position decideMove(GameState s){
+		return null;
+	}
+	
+}
